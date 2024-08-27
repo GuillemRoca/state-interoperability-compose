b35b224205d4457e076485f555f4d1c17b297e8c
antoniolg <antonio@devexperto.com>
2024-08-27 12:25:26 +0200

    State Holder exercise

--- app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
@@ -6,12 +6,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import io.devexpert.stateandinteroperability.data.Product
 import io.devexpert.stateandinteroperability.data.sampleProducts
+
+class MainState(searchTerm: String, products: List<Product>) {
+    var searchTerm by mutableStateOf(searchTerm)
+        private set
+    var products by mutableStateOf(products)
+        private set
+
+    fun onSearchChange(newSearchTerm: String) {
+        searchTerm = newSearchTerm
+        products = sampleProducts().filter { it.name.contains(searchTerm, true) }
+    }
+}
+
+@Composable
+fun rememberMainState(
+    searchTerm: String = "",
+    products: List<Product> = sampleProducts()
+): MainState {
+    return remember { MainState(searchTerm, products) }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +40,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Screen {
-                Column {
-                    var searchTerm by remember { mutableStateOf("") }
-                    val products = remember(searchTerm) {
-                        sampleProducts().filter { it.name.contains(searchTerm, true) }
-                    }
+                val state = rememberMainState()
 
-                    SearchBar(searchTerm = searchTerm, onSearchChange = { searchTerm = it })
+                Column {
+
+                    SearchBar(
+                        searchTerm = state.searchTerm,
+                        onSearchChange = state::onSearchChange
+                    )
 
                     ProductList(
-                        products = products,
+                        products = state.products,
                         onProductClick = { Log.d("MainActivity", "Product clicked: $it") }
                     )
                 }
