d3804f00dd1fa4fb63177ce8d71dbce1927ba343
antoniolg <antonio@devexperto.com>
2024-08-27 13:44:34 +0200

    SideEffect Sample

--- app/src/main/java/io/devexpert/stateandinteroperability/SearchBar.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/SearchBar.kt
@@ -1,13 +1,27 @@
 package io.devexpert.stateandinteroperability
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun SearchBar(searchTerm: String, onSearchChange: (String) -> Unit) {
+
+    var recomposeCount by remember { mutableIntStateOf(0) }
+
+    SideEffect {
+        recomposeCount++
+        Log.d("Compose", "Recomposition count: $recomposeCount")
+    }
+
     TextField(
         value = searchTerm,
         onValueChange = onSearchChange,
