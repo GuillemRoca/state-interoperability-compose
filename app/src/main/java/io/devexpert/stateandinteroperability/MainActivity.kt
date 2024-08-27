35592b8bf5126fc126d27e171698d63b1fff8ee9
antoniolg <antonio@devexperto.com>
2024-08-27 11:39:19 +0200

    rememberSaveable with listSaver

--- app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 
@@ -20,20 +22,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Screen {
-                ProductCounter()
+                ProductSortSelector()
             }
         }
     }
 }
 
+data class ProductSort(val field: String, val ascending: Boolean)
+
+val ProductSortListSaver = listSaver<ProductSort, Any>(
+    save = { listOf(it.field, it.ascending) },
+    restore = { ProductSort(it[0] as String, it[1] as Boolean) }
+)
+
 @Composable
-fun ProductCounter() {
-    var count by rememberSaveable { mutableIntStateOf(0) }
+fun ProductSortSelector() {
+    var sortOption by rememberSaveable(stateSaver = ProductSortListSaver) {
+        mutableStateOf(ProductSort("price", true))
+    }
 
     Column {
-        Text("Watched Products: $count")
-        Button(onClick = { count++ }) {
-            Text("Watch next product")
+        Text("Sort by: ${sortOption.field}")
+        Text("Ascending: ${sortOption.ascending}")
+        Button(onClick = {
+            sortOption = sortOption.copy(ascending = !sortOption.ascending)
+        }) {
+            Text("Change sort direction")
         }
     }
 }
