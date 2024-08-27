3921004ecc366be5a53e60c50771366575698d22
antoniolg <antonio@devexperto.com>
2024-08-27 13:35:52 +0200

    Custom CompositionLocal Sample

--- app/src/main/java/io/devexpert/stateandinteroperability/ProductList.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/ProductList.kt
@@ -7,21 +7,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import io.devexpert.stateandinteroperability.data.Product
 
+val LocalPriceStyle = compositionLocalOf { TextStyle() }
+
 @Composable
 fun ProductList(products: List<Product>, onProductClick: (Product) -> Unit) {
     LazyColumn {
         items(products) { product ->
-            CompositionLocalProvider(LocalTextStyle provides TextStyle(Color.Blue)) {
+            CompositionLocalProvider(
+                LocalTextStyle provides TextStyle(Color.Blue),
+                LocalPriceStyle provides TextStyle(Color.Red)
+            ) {
                 ProductItem(product, onProductClick)
             }
         }
@@ -37,6 +42,6 @@ fun ProductItem(product: Product, onProductClick: (Product) -> Unit) {
     ) {
         Text(product.name)
         Spacer(Modifier.weight(1f))
-        Text("${product.price}€")
+        Text("${product.price}€", style = LocalPriceStyle.current)
     }
 }
