0e41ba34857e92a0d4005f885c9b505824bd89b8
antoniolg <antonio@devexperto.com>
2024-08-27 13:18:03 +0200

    snapshotFlow Sample

--- app/src/main/java/io/devexpert/stateandinteroperability/SearchBar.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/SearchBar.kt
@@ -5,13 +5,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 
+@OptIn(FlowPreview::class)
 @Composable
 fun SearchBar(searchTerm: String, onSearchChange: (String) -> Unit) {
 
@@ -22,9 +30,23 @@ fun SearchBar(searchTerm: String, onSearchChange: (String) -> Unit) {
         Log.d("Compose", "Recomposition count: $recomposeCount")
     }
 
+    var localSearchTerm by remember { mutableStateOf(searchTerm) }
+
+    LaunchedEffect(searchTerm) {
+        snapshotFlow { localSearchTerm }
+            .debounce(500)
+            .filter { it.length > 2 }
+            .distinctUntilChanged()
+            .collect { newSearchTerm ->
+                if (newSearchTerm != searchTerm) {
+                    onSearchChange(newSearchTerm)
+                }
+            }
+    }
+
     TextField(
-        value = searchTerm,
-        onValueChange = onSearchChange,
+        value = localSearchTerm,
+        onValueChange = { localSearchTerm = it },
         label = { Text("Search Products") },
         modifier = Modifier.fillMaxWidth()
     )
