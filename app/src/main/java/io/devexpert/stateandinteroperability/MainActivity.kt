609798872cfa9afd073c0f949df68dca43298c26
antoniolg <antonio@devexperto.com>
2024-08-27 11:20:41 +0200

    remember sample

--- app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
@@ -4,6 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +19,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Screen {
-
+                ProductCounter()
             }
+        }
+    }
+}
+
+@Composable
+fun ProductCounter() {
+    var count by remember { mutableIntStateOf(0) }
+
+    Column {
+        Text("Productos vistos: $count")
+        Button(onClick = { count++ }) {
+            Text("Ver siguiente producto")
         }
     }
 }
