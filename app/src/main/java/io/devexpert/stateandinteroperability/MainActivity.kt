22f2d59c2c4a55adae1d992713fa3c9362797039
antoniolg <antonio@devexperto.com>
2024-08-27 12:36:21 +0200

    ViewModel: StateFlow

--- app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
@@ -6,27 +6,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.devexpert.stateandinteroperability.data.Product
 import io.devexpert.stateandinteroperability.data.sampleProducts
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
-    var searchTerm by mutableStateOf("")
-        private set
-    var products by mutableStateOf(sampleProducts())
-        private set
+    private val _state = MutableStateFlow(UiState())
+    val state: StateFlow<UiState> = _state.asStateFlow()
 
     fun onSearchChange(newSearchTerm: String) {
-        searchTerm = newSearchTerm
-        products = sampleProducts().filter { it.name.contains(searchTerm, true) }
+        _state.value = UiState(
+            searchTerm = newSearchTerm,
+            products = sampleProducts().filter {
+                it.name.contains(newSearchTerm, ignoreCase = true)
+            }
+        )
     }
+
+    data class UiState(
+        val searchTerm: String = "",
+        val products: List<Product> = sampleProducts()
+    )
 }
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,16 +42,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             Screen {
                 val vm = viewModel<MainViewModel>()
+                val state by vm.state.collectAsState()
 
                 Column {
 
                     SearchBar(
-                        searchTerm = vm.searchTerm,
+                        searchTerm = state.searchTerm,
                         onSearchChange = vm::onSearchChange
                     )
 
                     ProductList(
-                        products = vm.products,
+                        products = state.products,
                         onProductClick = { Log.d("MainActivity", "Product clicked: $it") }
                     )
                 }
