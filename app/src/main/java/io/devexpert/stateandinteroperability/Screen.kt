1e15a3650420dea482a4f3e697d4fb96b0257770
antoniolg <antonio@devexperto.com>
2024-08-27 11:12:57 +0200

    Initial commit

---
+++ app/src/main/java/io/devexpert/stateandinteroperability/Screen.kt
@@ -0,0 +1,35 @@
+package io.devexpert.stateandinteroperability
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import io.devexpert.stateandinteroperability.ui.theme.StateAndInteroperabilityTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Screen(
+    content: @Composable () -> Unit
+) {
+    StateAndInteroperabilityTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = { TopAppBar(title = { Text("State and Interoperability") }) }
+        ) { padding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding),
+                contentAlignment = Alignment.Center
+            ) {
+                content()
+            }
+        }
+    }
+}
