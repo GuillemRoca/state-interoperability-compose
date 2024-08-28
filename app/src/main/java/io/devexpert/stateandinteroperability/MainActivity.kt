e5fb078f5c05277acf3566171a2dfeee752be531
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-08-28 09:58:15 +0200

    First implementation simple product counter with remember

--- app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
@@ -4,6 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +24,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Screen {
+                ProductCounter()
+            }
+        }
+    }
 
+    @Composable
+    fun ProductCounter() {
+        var count by remember { mutableIntStateOf(0) }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "You have clicked $count times"
+            )
+            Button(onClick = { count++ }) {
+                Text("Click!")
             }
         }
     }
