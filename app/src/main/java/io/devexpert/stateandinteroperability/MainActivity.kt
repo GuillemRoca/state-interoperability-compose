ba4592afd7c5307dba0950ebc05bca41d9333d5f
Guillem Roca <10449477+GuillemRoca@users.noreply.github.com>
2024-08-28 10:07:30 +0200

    Implement list saver for custom data classes

--- app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -13,10 +12,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +23,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Screen {
-                ProductCounter()
+                ProductSortSelector()
             }
         }
     }
@@ -38,6 +37,30 @@ class MainActivity : ComponentActivity() {
             )
             Button(onClick = { count++ }) {
                 Text("Click!")
+            }
+        }
+    }
+
+    data class ProductSort(val field: String, val ascending: Boolean)
+
+    val productSortSaver = listSaver<ProductSort, Any>(
+        save = { listOf(it.field, it.ascending) },
+        restore = { ProductSort(it[0] as String, it[1] as Boolean) }
+    )
+
+    @Composable
+    fun ProductSortSelector() {
+        var sortOption by rememberSaveable(stateSaver = productSortSaver) {
+            mutableStateOf(ProductSort("price", true))
+        }
+
+        Column {
+            Text("Sort by: ${sortOption.field}")
+            Text("Ascending: ${sortOption.ascending}")
+            Button(onClick = {
+                sortOption = sortOption.copy(ascending = !sortOption.ascending)
+            }) {
+                Text("Change sort direction")
             }
         }
     }
