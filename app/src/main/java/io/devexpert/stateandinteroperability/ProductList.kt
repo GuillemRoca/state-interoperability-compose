1924e75a54913414e4b187ecfb98633a09c6342c
antoniolg <antonio@devexperto.com>
2024-08-29 13:20:41 +0200

    Load View from Composable

--- app/src/main/java/io/devexpert/stateandinteroperability/ProductList.kt
+++ app/src/main/java/io/devexpert/stateandinteroperability/ProductList.kt
@@ -1,5 +1,7 @@
 package io.devexpert.stateandinteroperability
 
+import android.view.View
+import android.widget.TextView
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,12 +17,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import io.devexpert.stateandinteroperability.data.Product
 
 val LocalPriceStyle = compositionLocalOf { TextStyle() }
 
 @Composable
+fun LegacyProductCounter(count: Int) {
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                id = View.generateViewId()
+                textSize = 18f
+                setPadding(16, 16, 16, 16)
+            }
+        },
+        update = { view ->
+            view.text = "Total Products: $count"
+        }
+    )
+}
+
+@Composable
 fun ProductList(products: List<Product>, onProductClick: (Product) -> Unit) {
+    LegacyProductCounter(products.size)
     LazyColumn {
         items(products) { product ->
             CompositionLocalProvider(
